0b5cd2baf9f41609a973a9992185ef2608498d27
Bayjid Alam Jubier <csejubier@gmail.com>
2026-02-01 17:29:26 +0600

    Add via files

---
+++ Linked_List/Doubly_Linked_List_Position_Return.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* prev;
+    Node* next;
+};
+
+int main(){
+    int n; cin>>n;
+    Node* head=NULL;
+    Node* tail=NULL;
+
+    for(int i=0;i<n;i++){
+        int x; cin>>x;
+        Node* node=new Node{x,tail,NULL};
+        if(!head) head=node;
+        else tail->next=node;
+        tail=node;
+    }
+
+    int key; cin>>key;
+    int pos=1;
+
+    for(Node* t=head;t;t=t->next,pos++){
+        if(t->data==key){
+            cout<<pos;
+            return 0;
+        }
+    }
+    cout<<-1;
+}
