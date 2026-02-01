1fd5e580acc58356ba9fad8dc6c3ab58938d5dc4
Bayjid Alam Jubier <csejubier@gmail.com>
2026-02-01 07:58:41 +0600

    Create Copy_String_One_To_Another.cpp

---
+++ String/Copy_String_One_To_Another.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char str1[100], str2[100];
+    cin.getline(str1, 100);
+    int i = 0;
+    while(str1[i] != '\0') {
+        str2[i] = str1[i];
+        i++;
+    }
+    str2[i] = '\0';
+    cout << str2;
+    return 0;
+}
