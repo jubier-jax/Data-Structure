9611ae0f729d30199f701c9f406cac7b05f16b78
Bayjid Alam Jubier <csejubier@gmail.com>
2026-02-01 13:10:16 +0600

    Add via files

---
+++ Linked_List/Singly_Linked_list_lenth_count.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int val) {
+        data = val;
+        next = NULL;
+    }
+};
+
+int length(Node* head) {
+    int count = 0;
+    Node* temp = head;
+    while (temp != NULL) {
+        count++;
+        temp = temp->next;
+    }
+    return count;
+}
+
+void display(Node* head) {
+    Node* temp = head;
+    while (temp != NULL) {
+        cout << temp->data << " -> ";
+        temp = temp->next;
+    }
+    cout << "NULL";
+}
+
+int main() {
+    Node* head = new Node(10);
+    head->next = new Node(20);
+    head->next->next = new Node(30);
+
+    display(head);
+    cout << endl;
+
+    cout << "Length: " << length(head) << endl;
+
+    return 0;
+}
