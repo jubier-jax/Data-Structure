3b8e6942d7c1914a14d5d154230dc48d4fb36a0c
Bayjid Alam Jubier <csejubier@gmail.com>
2026-02-01 13:16:36 +0600

    Add via files

---
+++ Linked_List/Singly_Linked_List_Sort.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int val) {
+        data = val;
+        next = NULL;
+    }
+};
+
+Node* sortLinkedList(Node* head) {
+    if (head == NULL)
+        return NULL;
+
+    bool swapped;
+    Node* ptr1;
+    Node* lptr = NULL;
+
+    do {
+        swapped = false;
+        ptr1 = head;
+
+        while (ptr1->next != lptr) {
+            if (ptr1->data > ptr1->next->data) {
+                int temp = ptr1->data;
+                ptr1->data = ptr1->next->data;
+                ptr1->next->data = temp;
+                swapped = true;
+            }
+            ptr1 = ptr1->next;
+        }
+        lptr = ptr1;
+    } while (swapped);
+
+    return head;
+}
+
+void display(Node* head) {
+    Node* temp = head;
+    while (temp != NULL) {
+        cout << temp->data << " -> ";
+        temp = temp->next;
+    }
+    cout << "NULL";
+}
+
+int main() {
+    Node* head = new Node(30);
+    head->next = new Node(10);
+    head->next->next = new Node(40);
+    head->next->next->next = new Node(20);
+
+    head = sortLinkedList(head);
+
+    display(head);
+
+    return 0;
+}
