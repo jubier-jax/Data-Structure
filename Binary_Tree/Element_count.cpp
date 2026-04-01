ead104974f66cb077f0fb0c00c22177f538bebc2
Bayjid Alam Jubier <csejubier@gmail.com>
2026-04-01 23:48:31 +0600

    Create Element_count.cpp

---
+++ Binary_Tree/Element_count.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+    Node(int val){ data=val; left=right=nullptr; }
+};
+
+Node* insert(Node* root,int val){
+    Node* newNode=new Node(val);
+    if(!root) return newNode;
+    queue<Node*> q;
+    q.push(root);
+    while(!q.empty()){
+        Node* temp=q.front(); q.pop();
+        if(!temp->left){ temp->left=newNode; return root; }
+        else q.push(temp->left);
+        if(!temp->right){ temp->right=newNode; return root; }
+        else q.push(temp->right);
+    }
+    return root;
+}
+
+int countNodes(Node* root){
+    if(!root) return 0;
+    return 1 + countNodes(root->left) + countNodes(root->right);
+}
+
+int main(){
+    Node* root=nullptr;
+    int n,val;
+    cin>>n;
+    for(int i=0;i<n;i++){ cin>>val; root=insert(root,val); }
+    cout << countNodes(root) << endl;
+    return 0;
+}
