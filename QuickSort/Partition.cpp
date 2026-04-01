b834b2933aad8a5c39da1691143a6635c3690394
Bayjid Alam Jubier <csejubier@gmail.com>
2026-04-01 22:44:23 +0600

    Create Partition.cpp

---
+++ QuickSort/Partition.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int partitionFunc(int arr[], int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1;
+
+    for(int j = low; j < high; j++) {
+        if(arr[j] < pivot) {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+
+    swap(arr[i + 1], arr[high]);
+    return i + 1;
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    int arr[100];
+
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int pi = partitionFunc(arr, 0, n - 1);
+
+    for(int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+}
