665d466e4fd4fe015ad1995dc377abf6b7aba4c2
Bayjid Alam Jubier <csejubier@gmail.com>
2026-04-01 23:54:38 +0600

    Create Node_count.cpp

---
+++ BST/Node_count.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+    Node(int val){ data=val; left=right=nullptr; }
+};
+
+Node* insert(Node* root,int val){
+    if(!root) return new Node(val);
+    if(val<root->data) root->left=insert(root->left,val);
+    else root->right=insert(root->right,val);
+    return root;
+}
+
+int countNodes(Node* root){
+    if(!root) return 0;
+    return 1 + countNodes(root->left) + countNodes(root->right);
+}
+
+int main(){
+    Node* root=nullptr;
+    int n,val;
+    cin>>n;
+    for(int i=0;i<n;i++){ cin>>val; root=insert(root,val); }
+    cout << countNodes(root) << endl;
+    return 0;
+}
