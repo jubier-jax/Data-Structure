5a6031fca2680dae0271c4da9f2303818602e6d6
Bayjid Alam Jubier <csejubier@gmail.com>
2026-04-02 00:49:41 +0600

    Create Build_Heap_From_Array.cpp

---
+++ Heap/Max-Heap/Build_Heap_From_Array.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void heapifyDownMax(vector<int>& h,int i,int n)
+{
+    while(i<n){
+        int l=2*i+1,r=2*i+2,largest=i;
+        if(l<n && h[l]>h[largest]) largest=l;
+        if(r<n && h[r]>h[largest]) largest=r;
+        if(largest==i) break;
+        swap(h[i],h[largest]);
+        i=largest;
+    }
+}
+
+void buildHeapMax(vector<int>& h)
+{
+    int n=h.size();
+    for(int i=n/2-1;i>=0;i--) heapifyDownMax(h,i,n);
+}
+
+int main(){
+    int n,v; cin>>n;
+    vector<int> h(n);
+    for(int i=0;i<n;i++) cin>>h[i];
+    buildHeapMax(h);
+    for(int x:h) cout<<x<<" "; cout<<endl;
+}
