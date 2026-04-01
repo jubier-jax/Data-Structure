fd62ea378a850e549aac085874e55a64c9e9bf62
Bayjid Alam Jubier <csejubier@gmail.com>
2026-04-02 00:58:34 +0600

    Create Get_Max_Size_And_Empty.cpp

---
+++ Heap/Max-Heap/Get_Max_Size_And_Empty.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int getMax(vector<int>& h){ return h.empty()?-1:h[0]; }
+int getSize(vector<int>& h){ return h.size(); }
+bool isEmpty(vector<int>& h){ return h.empty(); }
+
+int main()
+{ 
+  vector<int> h; 
+  int n,v; 
+  cin>>n; 
+  for(int i=0;i<n;i++)
+    { 
+      cin>>v; 
+      h.push_back(v); 
+    } 
+  cout<<getMax(h)<<endl; 
+  cout<<getSize(h)<<endl; 
+  cout<<(isEmpty(h)?"Yes":"No")<<endl; 
+}
