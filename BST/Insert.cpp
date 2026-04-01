369e2181fa45201d2431d226bf9de5b75b9b3890
Bayjid Alam Jubier <csejubier@gmail.com>
2026-04-01 23:30:56 +0600

    Create Insert.cpp

---
+++ BST/Insert.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+    Node(int val) { data = val; left = right = nullptr; }
+};
+
+Node* insert(Node* root, int val) {
+    if(!root) return new Node(val);
+    if(val < root->data) root->left = insert(root->left,val);
+    else root->right = insert(root->right,val);
+    return root;
+}
+
+int main() {
+    Node* root = nullptr;
+    int n,val;
+    cin >> n;
+    for(int i=0;i<n;i++) { cin >> val; root = insert(root,val); }
+    return 0;
+}
