55806a267d5726da2336afaa1989e848053b5165
Bayjid Alam Jubier <csejubier@gmail.com>
2026-04-01 23:41:43 +0600

    Create Postorder.cpp

---
+++ BST/Postorder.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+    Node(int val){ data=val; left=right=nullptr; }
+};
+
+Node* insert(Node* root,int val){
+    if(!root) return new Node(val);
+    if(val<root->data) root->left=insert(root->left,val);
+    else root->right=insert(root->right,val);
+    return root;
+}
+
+void postorder(Node* root){
+    if(!root) return;
+    postorder(root->left);
+    postorder(root->right);
+    cout<<root->data<<" ";
+}
+
+int main(){
+    Node* root=nullptr;
+    int n,val;
+    cin>>n;
+    for(int i=0;i<n;i++){ cin>>val; root=insert(root,val); }
+    postorder(root); cout<<endl;
+    return 0;
+}
