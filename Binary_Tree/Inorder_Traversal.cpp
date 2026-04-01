705629cae897db4bba7f01b40f52f90abd533cdc
Bayjid Alam Jubier <csejubier@gmail.com>
2026-04-01 23:22:52 +0600

    Create Inorder_Traversal.cpp

---
+++ Binary_Tree/Inorder_Traversal.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+    Node(int val) { data = val; left = right = nullptr; }
+};
+
+void inorder(Node* root) {
+    if(!root) return;
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+
+int main() {
+    Node* root = nullptr;
+    int n, val;
+    cin >> n;
+    for(int i=0;i<n;i++) { cin >> val; root = new Node(val); }
+    inorder(root);
+    return 0;
+}
