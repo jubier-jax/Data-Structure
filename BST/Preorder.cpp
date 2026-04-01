a71ef7c54a412d22fb556e3ca00de118fe8fe3df
Bayjid Alam Jubier <csejubier@gmail.com>
2026-04-01 23:39:50 +0600

    Create Preorder.cpp

---
+++ BST/Preorder.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+    Node(int val){ data=val; left=right=nullptr; }
+};
+
+Node* insert(Node* root,int val){
+    if(!root) return new Node(val);
+    if(val<root->data) root->left=insert(root->left,val);
+    else root->right=insert(root->right,val);
+    return root;
+}
+
+void preorder(Node* root){
+    if(!root) return;
+    cout<<root->data<<" ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+int main(){
+    Node* root=nullptr;
+    int n,val;
+    cin>>n;
+    for(int i=0;i<n;i++){ cin>>val; root=insert(root,val); }
+    preorder(root); cout<<endl;
+    return 0;
+}
