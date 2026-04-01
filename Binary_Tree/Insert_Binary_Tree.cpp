40d0ce3d555fad3840ca81d6ccd026ea7fdc325d
Bayjid Alam Jubier <csejubier@gmail.com>
2026-04-01 23:21:15 +0600

    Create Insert_Binary_Tree.cpp

---
+++ Binary_Tree/Insert_Binary_Tree.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+    Node(int val) {
+        data = val;
+        left = right = nullptr;
+    }
+};
+
+Node* insert(Node* root, int val) {
+    Node* newNode = new Node(val);
+    if(!root) return newNode;
+    queue<Node*> q;
+    q.push(root);
+    while(!q.empty()) {
+        Node* temp = q.front();
+        q.pop();
+        if(!temp->left) { temp->left = newNode; return root; }
+        else q.push(temp->left);
+        if(!temp->right) { temp->right = newNode; return root; }
+        else q.push(temp->right);
+    }
+    return root;
+}
+
+int main() {
+    Node* root = nullptr;
+    int n, val;
+    cin >> n;
+    for(int i=0;i<n;i++) {
+        cin >> val;
+        root = insert(root, val);
+    }
+    return 0;
+}
