48875840cd3ded1bef94dd0d6daf48f97b47fdae
Bayjid Alam Jubier <csejubier@gmail.com>
2026-04-01 22:21:27 +0600

    Create Pop.cpp

---
+++ Stack/Pop.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+#define MAX 100
+int stackArr[MAX];
+int top = -1;
+
+int main() {
+    int n, x;
+    cin >> n;
+
+    for(int i = 0; i < n; i++) {
+        cin >> x;
+        stackArr[++top] = x;
+    }
+
+    if(top == -1) {
+        cout << "Underflow";
+    } else {
+        cout << stackArr[top--] << endl;
+    }
+
+    for(int i = top; i >= 0; i--) {
+        cout << stackArr[i] << " ";
+    }
+}
