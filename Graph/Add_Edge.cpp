f5a379dfd7a1b0e02ea6dfbcc411518f961d416c
Bayjid Alam Jubier <csejubier@gmail.com>
2026-04-02 08:17:29 +0600

    Create Add_Edge.cpp

---
+++ Graph/Add_Edge.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    int n,m,u,v;
+    cin>>n>>m;
+    vector<vector<int>> adj(n);
+    for(int i=0;i<m;i++){
+        cin>>u>>v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    cin>>u>>v;
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+    for(int i=0;i<n;i++){
+        for(int x:adj[i]) cout<<x<<" ";
+        cout<<endl;
+    }
+}
