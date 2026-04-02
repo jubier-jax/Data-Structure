6585a60a80c17718d52af4aede24b181f8971480
Bayjid Alam Jubier <csejubier@gmail.com>
2026-04-02 08:03:36 +0600

    Create Insert.cpp

---
+++ Heap/Min-Heap/Insert.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void heapifyUpMin(vector<int>& h,int i){
+    while(i>0){
+        int p=(i-1)/2;
+        if(h[p]>h[i]) swap(h[p],h[i]);
+        else break;
+        i=p;
+    }
+}
+
+void insertMin(vector<int>& h,int val){
+    h.push_back(val);
+    heapifyUpMin(h,h.size()-1);
+}
+
+int main(){ 
+  vector<int> h; 
+  int n,v; 
+  cin>>n; 
+  for(int i=0;i<n;i++)
+  { 
+    cin>>v; 
+    insertMin(h,v); 
+  } 
+  for(int x:h) cout<<x<<" "; 
+  cout<<endl; 
+}
