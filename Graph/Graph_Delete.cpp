3d074bdf4212337b7b3a0628e9ef65eab94b9777
Bayjid Alam Jubier <csejubier@gmail.com>
2026-04-02 08:23:05 +0600

    Create Graph_Delete.cpp

---
+++ Graph/Graph_Delete.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    int n,m,u,v;
+    cin>>n>>m;
+    vector<vector<int>> adj(n);
+    for(int i=0;i<m;i++){
+        cin>>u>>v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    adj.clear();
+    return 0;
+}
